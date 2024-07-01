4bc7d5f412fd894f9d59f022956827e5c802c135
VAtulKumar1 <atulkumarverma304@gmail.com>
2024-07-01 23:56:40 +0530

    Added all admin routes

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,31 +1,146 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const app = express();
 
 app.use(express.json());
 
-let ADMINS = [];
-let USERS = [];
-let COURSES = [];
+let secret = "Tn9Qx2Ks7Mf3Jw4Pz6Rv";
+let secretUser = "Tn9Qx2Ks7Mf3Jw4Pz6Rv";
+let token = "";
+function generateJwt(payload){
+    token = jwt.sign(payload,secret,{expiresIn:'1h'});
+    return token;
+}
 
-// Admin routes
-app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+let tokenUser = "";
+function generateJwtUser(payload){
+  tokenUser = jwt.sign(payload,secretUser,{expiresIn:'1h'});
+    return tokenUser;
+}
+
+function authenticateJwt(req,res,next){
+  const token = req.headers.split(" ")[1];
+  if(!token){
+    res.status(401).Json({message: "Please provide a valid token"});
+  }
+  jwt.verify(token,secret,(err,user)=>{
+    if(err){
+      res.status(403).Json({message: "unauthorized"});
+    }else{
+      req.user = user;
+      next();
+    }
+    
+  });
+}
+
+
+
+function authenticateJwtUser(req,res,next){
+  const token = req.headers.split(" ")[1];
+  if(!token){
+    res.status(401).Json({message: "Please provide a valid token"});
+  }
+  jwt.verify(token,secretUser,(err,user)=>{
+    if(err){
+      res.status(403).Json({message: "unauthorized"});
+    }else{
+      req.user = user;
+      next();
+    }
+    
+  });
+}
+
+
+
+
+const adminSchema = mongoose.Schema({
+  userName:{ type:String, required:true, unique:true},
+  password:{ type:String ,required:true}
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+const userSchema =mongoose.Schema({
+  userName:{ type:String, required:true, unique:true},
+  password:{ type:String ,required:true},
+  purchasedCourses: { type: mongoose.Schema.Types.ObjectId, ref:"Course"}
+})
+
+const courseSchema = mongoose.Schema({
+    title: {type:String, required:true},
+    description: String,
+    price: Number,
+    imageLink: String,
+    published: {type:Boolean,default:true}
 });
 
-app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+
+const User = mongoose.model("User",userSchema);
+const Admin = mongoose.model("Admin",adminSchema);
+const Course = mongoose.model("Course",courseSchema);
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/CourseApp");
+
+
+
+
+app.post('/admin/signup', async (req, res) => {
+  const {userName,password} = {...req.body}
+
+  const existingUser = await Admin.findOne({userName});
+  if(existingUser){
+    res.status(409).json({message : "user already exisits"});
+  }
+
+  const admin = new Admin({userName,password});
+  await admin.save(user);
+
+
+  let token = await generateJwt({userName});
+  res.status(201).json({message:"Admin Created Successfully",
+    token}
+  );
+
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+app.post('/admin/login',async (req, res) => {
+
+  const {userName,password} = req.body;
+
+  const existingUser = await Admin.findOne({userName,password});
+  if(!existingUser){
+    res.status(403).Json({message:"unauthorized access"});
+  }
+
+  let token = await generateJwt({userName});
+  res.status(200).Json({message: "admin logged in successfuly",token});
+
 });
 
-app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+app.post('/admin/courses',authenticateJwt, async(req, res) => {
+  const course = new Course(req.body);
+  await course.save(course);
+  res.status(201).json({message:"course created successfully"});
+  
+});
+
+app.put('/admin/courses/:courseId', authenticateJwt, async(req, res) => {
+  const courseId = req.params.courseId;
+  const updatedCourse = new Course(req.body);
+  const course =await Course.findByIdAndUpdate(courseId,updatedCourse,{new:true},(err,course)=>{
+    if(err){
+      res.status(404).Json({message:"course not found"});
+    }
+    res.status(200).Json(course);
+
+  }) 
+});
+
+app.get('/admin/courses',authenticateJwt, async (req, res) => {
+  const courses = await Course.find({});
+  res.status(200).Json(courses);
 });
 
 // User routes
@@ -52,3 +167,7 @@ app.get('/users/purchasedCourses', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
+
+
+
+
